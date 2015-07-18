b4a9392d665ac3bbbe65e78062f8e62c28bc37eb
Balint Erdi <balint.erdi@gmail.com>
2015-07-18 18:41:20 +0200

    Fix reselecting the same option (with different text)
    I wonder if this.rerender() should not set the input's value attribute correctly?

--- app/components/rr-combobox-input.js
+++ app/components/rr-combobox-input.js
@@ -4,9 +4,17 @@ export default Ember.Component.extend({
   tagName: 'input',
   attributeBindings: 'value type placeholder'.w(),
 
+  registerWithCombobox: Ember.on('didInsertElement', function() {
+    this.get('parentView').registerInput(this);
+  }),
 
   filterItems: Ember.on('input', function() {
     const value = this.$().val();
     this.get('on-change')(value);
-  })
+  }),
+
+  updateValue() {
+    const value = this.get('value');
+    this.$().val(value);
+  },
 });
--- app/components/rr-combobox.js
+++ app/components/rr-combobox.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isDropdownVisible: false,
+  input: null,
+
+  registerInput(input) {
+    this.set('input', input);
+  },
 
   toggleDropdown() {
     this.toggleProperty('isDropdownVisible');
@@ -11,6 +16,12 @@ export default Ember.Component.extend({
     selectItem(item) {
       this.get('on-select')(item);
       this.set('isDropdownVisible', false);
+      //NOTE: When the property the input's value attr is bound to does not change,
+      // (e.g you select the same item after backspacing a few chars), the rr-combobox-input
+      // does not display the selected item's label. This is a workaround for that scenario,
+      // though I wish there was a solution that does not involve the registerInput process.
+      // Also, this.rerender() does not rerender the input's value, though I'd think it should.
+      this.get('input').updateValue();
     },
 
     inputDidChange(value) {
