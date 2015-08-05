995d3f945a44a62b5fd7fc2442d25927274ea518
Balint Erdi <balint.erdi@gmail.com>
2015-08-05 18:15:31 +0200

    Make input inherit from Ember.TextField

--- app/components/rr-combobox-input.js
+++ app/components/rr-combobox-input.js
@@ -1,8 +1,6 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
-  tagName: 'input',
-  attributeBindings: 'value type placeholder'.w(),
+export default Ember.TextField.extend({
 
   registerWithCombobox: Ember.on('didInsertElement', function() {
     this.get('parentView').registerInput(this);
@@ -11,9 +9,6 @@ export default Ember.Component.extend({
   valueDidChange: Ember.on('input', function() {
     const value = this.$().val();
     this.get('on-change')(value);
-    // TODO: If there is a selected item, use input.setSelectedRange to select
-    // the "inferred" text so that the component does not get in the way of
-    // typing. See ic-autocomplete/autocomplete.js/autocompleteText on how to do that.
   }),
 
   updateValue() {
