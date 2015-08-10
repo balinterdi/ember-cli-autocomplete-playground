12ca1c18bd799455b3fd8c323388f1c4b37ab6e7
Balint Erdi <balint.erdi@gmail.com>
2015-08-10 16:41:20 +0200

    Make up & down arrows cycle through dropdown options and enter to select focused one

--- app/components/rr-combobox-option.js
+++ app/components/rr-combobox-option.js
@@ -2,11 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'li',
+  classNames: 'combobox-option',
+  classNameBindings: 'isActive:active isFocused:focused'.w(),
 
   list: null,
   id: null,
   label: null,
   item: null,
+  activeId: null,
+
+  isFocused: false,
+
+  isActive: Ember.computed('id', 'activeId', function() {
+    return this.get('id') === this.get('activeId');
+  }),
+
+  combobox: Ember.computed.reads('list.combobox'),
 
   registerWithList: Ember.on('didInsertElement', function() {
     this.get('list').registerOption(this);
@@ -19,5 +30,13 @@ export default Ember.Component.extend({
   toggleDropdown: Ember.on('click', function() {
     const item = this.get('item');
     this.get('on-click')(item, this.get('label'));
-  })
+  }),
+
+  focus: function() {
+    this.set('isFocused', true);
+  },
+
+  blur: function() {
+    this.set('isFocused', false);
+  },
 });
--- app/components/rr-combobox.js
+++ app/components/rr-combobox.js
@@ -10,6 +10,7 @@ export default Ember.Component.extend({
   list: null,
 
   isBackspacing: false,
+  options: Ember.computed.reads('list.options'),
 
   registerInput(input) {
     this.set('input', input);
@@ -23,12 +24,76 @@ export default Ember.Component.extend({
     this.toggleProperty('isDropdownVisible');
   },
 
+  openDropdown() {
+    this.set('isDropdownVisible', true);
+  },
+
   keydownMap: {
-    8: 'startBackspacing' // backspace
+    8:  'startBackspacing', // backspace
+    13: 'selectOption',  // return
+    38: 'focusPrevious', // up key
+    40: 'focusNext', // down key
   },
 
   startBackspacing: function() {
     this.set('isBackspacing', true);
+  },
+
+  selectOption: function() {
+    this.set('isDropdownVisible', false);
+  },
+
+  focusPrevious: function(event) {
+    event.preventDefault();
+    const focused = this.get('focusedOption');
+    if (!focused) {
+      return;
+    }
+
+    let index = this.get('options').indexOf(focused);
+    if (this.get('isDropdownVisible')) {
+      index = index - 1;
+    }
+    this.focusOptionAtIndex(index);
+  },
+
+  focusNext: function(event) {
+    //TODO: Refactor with focusPrevious
+    event.preventDefault();
+    let index = 0;
+    const focused = this.get('focusedOption');
+    if (!focused) {
+      return;
+    }
+
+    index = this.get('options').indexOf(focused);
+    if (this.get('isDropdownVisible')) {
+      index = index + 1;
+    }
+    this.focusOptionAtIndex(index);
+  },
+
+  focusOptionAtIndex: function(index) {
+    const options = this.get('options');
+    if (index === -1) {
+      index = options.get('length') - 1;
+    } else if (index === options.get('length')) {
+      index = 0;
+    }
+    const option = this.get('options').objectAt(index);
+    if (!option) {
+      return;
+    }
+    this.focusOption(option);
+  },
+
+  focusOption: function(option) {
+    const focused = this.get('focusedOption');
+    if (focused) {
+      focused.blur();
+    }
+    this.set('focusedOption', option);
+    option.focus();
   },
 
   handleKeydown: Ember.on('keyDown', function(event) {
@@ -58,6 +123,7 @@ export default Ember.Component.extend({
         const firstOption = this.get('list.firstOption');
         if (firstOption) {
           const autocompletedLabel = firstOption.get('label');
+          this.set('focusedOption', firstOption);
           this.get('on-select')(firstOption.get('item'));
           this.set('inputValue', autocompletedLabel);
           Ember.run.next(this, () => {
